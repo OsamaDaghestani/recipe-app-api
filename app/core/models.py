169b7d6b9130e1ea88faa71988404d2aba7f4e46
Osama Daghestani <Daghestaniosama@gmail.com>
2020-11-28 14:59:49 +0300

    error

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,23 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
-# Create your models here.
+                                        PermissionsMixin
+from django.conf import settings
 
 
-class UserManger(BaseUserManager):
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
+
+
+class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new User"""
+        """Creates and saves a new user"""
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -17,7 +27,7 @@ class UserManger(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """creats and saves a new super user """
+        """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -27,12 +37,12 @@ class UserManger(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """ Custom User Model"""
+    """Custom user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserManger()
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
 class ModelsTests(TestCase):
     def test_creat_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
@@ -14,11 +13,10 @@ class ModelsTests(TestCase):
 
         self.assertEqual(user.email, email),
         self.assertTrue(user.check_password(password))
-
     def test_new_user_email_normalized(self):
         """ test the email for a new user is normalized """
         email = 'test@LONDONAPPDEV.COM'
-        user = get_user_model().objects.create_user(email, 'test123')
+        user = get_user_model().objects.create_user(email,'test123')
 
         self.assertEqual(user.email, email.lower())
 
